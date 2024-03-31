bff5b0f85ee254132286548c6132a1146e88c7d8
simbadz <simbadez@yandex.ru>
2024-03-31 18:26:51 +0300

    create logic of first screen

--- app/src/main/java/com/example/calculator/cafeorigin/MainActivity.java
+++ app/src/main/java/com/example/calculator/cafeorigin/MainActivity.java
@@ -4,14 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, OrderDetailActivity.class);
+        initViews();
+
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = editTextName.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+                if (userName.isEmpty() || password.isEmpty()) {
+
+                    Toast.makeText(
+                            MainActivity.this,
+                            R.string.error_fields_empty,
+                            Toast.LENGTH_SHORT
+
+                    ).show();
+
+                } else {
+                    launchNextScreen(userName);
+
+
+                }
+            }
+        });
+    }
+
+    private void launchNextScreen(String userName) {
+        Intent intent = MakeOrderActivity.newIntent(this, userName);
         startActivity(intent);
+    }
+
+    private void initViews()  {
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonSignIn = findViewById(R.id.buttonSignIn);
     }
 }
--- app/src/main/java/com/example/calculator/cafeorigin/MakeOrderActivity.java
+++ app/src/main/java/com/example/calculator/cafeorigin/MakeOrderActivity.java
@@ -1,7 +1,10 @@
  package com.example.calculator.cafeorigin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MakeOrderActivity extends AppCompatActivity {
@@ -10,5 +13,11 @@ public class MakeOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_order);
+    }
+
+    public static Intent newIntent(Context context, String userName) {
+        Intent intent = new Intent(context, MakeOrderActivity.class);
+        intent.putExtra("userName", userName);
+        return intent;
     }
 }
