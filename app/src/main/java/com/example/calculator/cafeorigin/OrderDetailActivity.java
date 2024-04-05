3ba105a55e27a78c0b4fd0844312863a440b18ee
simbadz <simbadez@yandex.ru>
2024-04-05 10:44:37 +0300

    create logic of third screen

--- app/src/main/java/com/example/calculator/cafeorigin/OrderDetailActivity.java
+++ app/src/main/java/com/example/calculator/cafeorigin/OrderDetailActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OrderDetailActivity extends AppCompatActivity {
 
@@ -13,11 +14,33 @@ public class OrderDetailActivity extends AppCompatActivity {
     private static final String EXTRA_DRINK_TYPE = "drinkType";
     private static final String EXTRA_ADDITIVIES = "additivies";
 
+
+    private TextView textViewName;
+    private TextView textViewDrink;
+    private TextView textViewDrinkType;
+    private TextView textViewAdditivies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_detail);
+        initViews();
+
+        Intent intent = getIntent();
+        textViewName.setText(intent.getStringExtra(EXTRA_USER_NAME));
+        textViewDrink.setText(intent.getStringExtra(EXTRA_DRINK));
+        textViewDrinkType.setText(intent.getStringExtra(EXTRA_DRINK_TYPE));
+        textViewAdditivies.setText(intent.getStringExtra(EXTRA_ADDITIVIES));
     }
+
+    private void initViews() {
+        textViewName = findViewById(R.id.textViewName);
+        textViewDrink = findViewById(R.id.textViewDrink);
+        textViewDrinkType = findViewById(R.id.textViewDrinkType);
+        textViewAdditivies = findViewById(R.id.textViewAdditivies);
+
+    }
+
+
 
     public static Intent newIntent(
             Context context,
